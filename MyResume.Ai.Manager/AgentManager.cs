08c5b9fa976b6c6b827e24b1fd3d3c653aa90a08
Sourav Roy Choudhury <sourav.roychoudhury@outlook.com>
2026-02-16 11:27:52 -0500

    working version of ai code

--- MyResume.Ai.Manager/AgentManager.cs
+++ MyResume.Ai.Manager/AgentManager.cs
@@ -1,9 +1,7 @@
 ﻿using Azure;
 using Azure.AI.OpenAI;
-using Azure.AI.OpenAI.Chat;
 using Azure.Core;
 using MyResume.Model.AiModels;
-using OpenAI.Chat;
 using Azure.Identity;
 using System;
 using System.Collections.Generic;
@@ -16,13 +14,13 @@ namespace MyResume.Ai.Manager
 {
     public class AgentManager
     {
-        public async Task<string> GetResponse( string promt)
+        public async Task<string> GetResponse(string promt)
         {
             try
             {
                 string openAiEndpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT")
                     ?? throw new ArgumentNullException("OpenAI endpoint is not provided in config file.")
-                    ,openAiKey= Environment.GetEnvironmentVariable("AZURE_OPENAI_KEY")
+                    , openAiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_KEY")
                     ?? throw new ArgumentNullException("OpenAI key is not provided in config file.")
                     , embeddingDeployment = Environment.GetEnvironmentVariable("EMBEDDING_MODEL")
                     ?? throw new ArgumentNullException("OpenAI key is not provided in config file.")
@@ -35,37 +33,51 @@ namespace MyResume.Ai.Manager
                     , searchIndex = Environment.GetEnvironmentVariable("AI_SEARCH_INDEX")
                     ?? throw new ArgumentNullException("OpenAI key is not provided in config file.");
 
-                var openAiClient = new AzureOpenAIClient(new Uri(openAiEndpoint)
+                var openAiClient = new OpenAIClient(new Uri(openAiEndpoint)
                     , new AzureKeyCredential(openAiKey));
-                var chatClient=openAiClient.GetChatClient(chatDeployment);
-#pragma warning disable AOAI001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-                var auth = DataSourceAuthentication.FromApiKey(searchKey);
-#pragma warning restore AOAI001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-#pragma warning disable AOAI001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-                var searchClient = new AzureSearchChatDataSource()
+
+                var searchClient = new AzureSearchChatExtensionConfiguration()
                 {
-                    Endpoint = new Uri(searchEndpoint),
-                    Authentication= auth,
+                    SearchEndpoint = new Uri(searchEndpoint),
+                    Authentication = new OnYourDataApiKeyAuthenticationOptions(searchKey),
                     IndexName = searchIndex,
-                };
-#pragma warning restore AOAI001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
-                var option=new ChatCompletionOptions()
+                };
+
+                var extensionOptions = new AzureChatExtensionsOptions()
                 {
-                    Temperature = 0.7f,
-                    TopP = 0.9f,
-                    FrequencyPenalty = 0.0f,
-                    PresencePenalty = 0.0f
+                    Extensions = { searchClient }
                 };
 
-#pragma warning disable AOAI001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-                option.AddDataSource(searchClient);
-#pragma warning restore AOAI001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
-                var messages =new[]{(ChatMessage)( new SystemChatMessage("")) ,  new UserChatMessage(promt) };
+                var option = new ChatCompletionsOptions()
+                {
+                    Temperature = 0.2f,
+                    DeploymentName = chatDeployment
+                };
 
-                var respons= chatClient.CompleteChat(messages,option);
-                var reply = respons.Value.Content[0].Text;
+                option.AzureExtensionsOptions = extensionOptions;
+
+                option.Messages.Add(new ChatRequestSystemMessage(@"You are a helpful assistant. Use the data retrieved from the Azure Search index to answer questions.
+You are an human like AI assistant that helps people find information about skill, career and experience of Sourav Roy Choudhury. you will respond impersonating as Sourav Roy Choudhury.
+
+Your Azure Search index source contains resume for Sourav. You have Sourav's resume as data. Use that to respond people. Use the data to talk about career and job experience, future vision as lead AI developer and solution architect. Always emphasize how good Sourav is in each of the technology mentioned.
+
+The resume data has educational background of Sourav along with company and job experience and job location and project timeline, when asked, mention all details. When mentioning technical experience always align to resume data.
+
+When ever you are talking about technical skill and experience, never mention those skills which are not mentioned in resume. 
+
+Never mention that you are pulling data from resume or from your general knowledge.
+
+When asked for vision mention about AI vision. When ever responding from resume data, use human like tone first, unless asked for specific type of response. Don't always use bullate points, rather create paragraphs with human like speech. Don't include paragraph headers. Create responses as you are giving technical interview.
+
+At first try to to give short answer and ask if you need to explain. When asked, give more explanation. But always try to respond as a human giving technical interview.
+
+Don't respond 'The requested information is not available in the retrieved data. Please try another query or topic.' rather create response messages such, oops i dont know that. I am sorry i am not aware of that etc."));
+
+                option.Messages.Add(new ChatRequestUserMessage(promt));
+                var respons = openAiClient.GetChatCompletions(option);
+                var reply = respons.Value.Choices[0].Message.Content;
 
                 return reply;
             }
